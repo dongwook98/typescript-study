b6500458dbf32245408d0a11bd6bf508f5514575
강동욱 <ehddnr8563@gmail.com>
2024-01-09 23:11:50 +0900

    객체지향적으로 커피기계 만들기 (static 사용) - 코드 수정

--- 3-oop/3-2-class.ts
+++ 3-oop/3-2-class.ts
@@ -26,21 +26,16 @@
      */
     static BEANS_GRAM_PER_SHOT: number = 18; // class level
     coffeeBeans: number = 0; // instance (object) level
-    hasSugar: boolean = false;
 
-    constructor(coffeeBeans: number, hasSugar: boolean = false) {
+    constructor(coffeeBeans: number) {
       this.coffeeBeans = coffeeBeans;
-      this.hasSugar = hasSugar;
     }
 
-    static makeCoffeeMachine(
-      coffeeBeans: number,
-      hasSugar: boolean
-    ): CoffeeMachine {
-      return new CoffeeMachine(coffeeBeans, hasSugar);
+    static makeCoffeeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
     }
 
-    makeCoffee(shots: number, hasSugar: boolean): CoffeeCup {
+    makeCoffee(shots: number, hasSugar: boolean = false): CoffeeCup {
       if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
         throw new Error(
           `현재 원두:${this.coffeeBeans} 원두가 부족합니다. 원두를 더 넣어주세요.`
@@ -57,15 +52,17 @@
   const coffeeMachine = new CoffeeMachine(18);
   console.log(coffeeMachine);
 
-  const coffeeMachine2 = new CoffeeMachine(32, true);
+  const coffeeMachine2 = new CoffeeMachine(32);
   console.log(coffeeMachine2);
 
   /**
    * class level 메서드로 만들어두어서
    * 사용자가 constructor를 호출하지 않고도 coffeeMachine을 만듬
    */
-  const coffeeMachine3 = CoffeeMachine.makeCoffeeMachine(18, true);
+  const coffeeMachine3 = CoffeeMachine.makeCoffeeMachine(18);
   console.log(coffeeMachine3);
+  const coffee3 = coffeeMachine3.makeCoffee(1, true);
+  console.log(coffee3);
 
   // static을 사용한 class level 메서드 예제
   console.log(Math.abs(-5));
