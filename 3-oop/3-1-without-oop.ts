c8034f16a59e09a38ddd747e5c6897f5981ae45c
강동욱 <ehddnr8563@gmail.com>
2024-01-13 02:47:15 +0900

    절차형 프로그래밍

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -1,32 +1,27 @@
 {
   type CoffeeCup = {
     shots: number;
-    hasSugar: boolean;
+    hasMilk?: boolean;
   };
 
-  const BEANS_GRAM_PER_SHOT = 18;
+  /**
+   * 절차형 프로그래밍은 필요한 상수, 데이터, 함수들이 밖에서 서로 뒹굴고 있음
+   */
+  const BEANS_GRAM_PER_SHOT: number = 18;
 
-  let coffeeBeans = 0;
-  function makeCoffee(shots: number, hasSugar = false): CoffeeCup {
-    if (coffeeBeans < shots * BEANS_GRAM_PER_SHOT) {
-      throw new Error(
-        `현재 원두:${coffeeBeans} 원두가 부족합니다. 원두를 더 넣어주세요.`
-      );
+  let coffeeBeansGram: number = 0;
+  function makeCoffee(shots: number): CoffeeCup {
+    if (coffeeBeansGram < shots * BEANS_GRAM_PER_SHOT) {
+      throw new Error('원두가 부족합니다. 원두를 더 넣어주세요.');
     }
-    coffeeBeans -= shots * BEANS_GRAM_PER_SHOT;
+    coffeeBeansGram -= shots * BEANS_GRAM_PER_SHOT;
     return {
       shots,
-      hasSugar,
+      hasMilk: false,
     };
   }
 
-  coffeeBeans += 2 * BEANS_GRAM_PER_SHOT; // 2샷을 위한 원두 추가
-  const coffee = makeCoffee(2); // 2샷, 설탕X
+  coffeeBeansGram += 2 * BEANS_GRAM_PER_SHOT;
+  const coffee = makeCoffee(2);
   console.log(coffee);
-
-  coffeeBeans += 4 * BEANS_GRAM_PER_SHOT; // 4샷을 위한 원두 추가
-  const coffee2 = makeCoffee(4, true); // 4샷, 설탕 추가
-  console.log(coffee2);
-
-  const coffee3 = makeCoffee(1); // Error: 현재 원두:0 원두가 부족합니다. 원두를 더 넣어주세요.
 }
